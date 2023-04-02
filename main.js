52ddcf8550deb12c68090971933732287d66479b
Gurdit09 <90044541+Gurdit09@users.noreply.github.com>
2023-04-02 10:15:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+img = "";
+status = "";
+objects = [];
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(380,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380)
+    video.hide()
+}
+
+function draw()
+{
+    image(video,0,0,380,380);
+
+       if(status != "")
+       {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video,gotResult);
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Baby is detected";
+            
+
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+       }
+
+    
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status : Detecting baby";
+}
+
+for (i = 0; i < objects.length; i++)
+{
+    if (objects[i].label == "person")
+    {
+        document.getElementById("status").innerHTML = "Status : Baby Detected"
+        stopSound("emergency_alert")
+    }
+    else
+    {
+       document.getElementById("status").innerHTML = "Status : baby not detected"
+       playSound("emergency_alert")
+    }
+}
+
